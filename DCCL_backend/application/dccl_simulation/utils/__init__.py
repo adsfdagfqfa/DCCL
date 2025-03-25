02ee8bd9149f4580d795d1e811f9b6873b05d18e
adsfdagfqfa <2151767@tongji.edu.cn>
2025-03-25 05:59:53 +0000

    修改后端Bug

--- DCCL_backend/application/dccl_simulation/utils/__init__.py
+++ DCCL_backend/application/dccl_simulation/utils/__init__.py
@@ -5,5 +5,3 @@ from .operator_reflectivity import operator_reflectivity
 from .operator_transmissivity import operator_transmissivity
 from .para_FFT import para_FFT
 
-def format_string(value):
-    return "data: "+str(value)+"\n\n"
