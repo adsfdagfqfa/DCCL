07ac57ba755b0bb73fa9b9cca8b720af2d2dc0b1
adsfdagfqfa <134774045+adsfdagfqfa@users.noreply.github.com>
2025-04-07 00:55:22 +0800

    更新目录文件结构，删除outputResult组件目录

--- DCCL_frontend/src/views/mainView.vue
+++ DCCL_frontend/src/views/mainView.vue
@@ -16,9 +16,21 @@
       <!-- <test></test> -->
     </div>
     <div>
-      <OutputResult></OutputResult>
+      <!-- <OutputResult></OutputResult> -->
+      <Element/>
+      <el-tabs v-model="activeName" @tab-click="handleClick">
+        <el-tab-pane label="常数" name="constant">
+          <Constant/>
+        </el-tab-pane>
+        <el-tab-pane label="参数" name="parameter">
+          <Parameter/>
+        </el-tab-pane>
+        <el-tab-pane label="仿真" name="simulation">
+          <Simulation/>
+        </el-tab-pane>
+      </el-tabs>
     </div>
-    <PyPlot></PyPlot>
+    <!-- <PyPlot></PyPlot> -->
   </div>
 </template>
 
@@ -30,7 +42,15 @@ import OutputResult from '@/components/outputResult/index.vue';
 import Resonator from '@/components/resonator/index.vue';
 import { onMounted } from 'vue';
 import PyPlot from '@/components/pyPlot/index.vue';
+import Element from '@/components/element/index.vue';
+import Constant from '@/components/outputResult/constant.vue';
+import Parameter from '@/components/outputResult/parameter.vue';
+import Simulation from '@/components/outputResult/simulation.vue';
 onMounted(async()=>{
   console.log("mainView","挂载")
 })
+const tabs=[]
+function handleClick(tab, event) {
+  console.log(tab, event);
+}
 </script>
