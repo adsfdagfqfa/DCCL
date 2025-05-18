1c1957faf537897728e9bedae10cd172bdb215db
adsfdagfqfa <2151767@tongji.edu.cn>
2025-05-18 10:23:30 +0000

    修改

--- DCCL_backend/application/dccl_simulation/utils/cal_field_transition.py
+++ DCCL_backend/application/dccl_simulation/utils/cal_field_transition.py
@@ -18,11 +18,9 @@ def cal_field_transition(U_pre, H, B_aper, B_lens):
     # # 到达面光场
     # U = fftshift(ifft2(ifftshift(H * B))) * B_lens
     #scipy.fft.set_global_backend(cufft)
-    B_aper_dense = B_aper.toarray()#转为密集型矩阵
-    B_lens_dense = B_lens.toarray()
-    B = cufft.fftshift(cufft.fft2(cufft.ifftshift(U_pre * B_aper_dense)))
-    U = cufft.fftshift(cufft.ifft2(cufft.ifftshift(H * B))) * B_lens_dense
-    del B_aper_dense
-    del B_lens_dense
+   
+    B = cufft.fftshift(cufft.fft2(cufft.ifftshift(U_pre * B_aper)))
+    U = cufft.fftshift(cufft.ifft2(cufft.ifftshift(H * B))) * B_lens
+    del B
     cp.get_default_memory_pool().free_all_blocks()  
     return U
