677530e0553ddba96ef408e3299dcfed6020b00c
adsfdagfqfa <134774045+adsfdagfqfa@users.noreply.github.com>
2025-06-18 23:52:21 +0800

    修改aperture部分

---
+++ DCCL_backend/application/services/dccl_simulation/aperture/aperture_calculation.py
@@ -0,0 +1,77 @@
+import numpy as np
+import cupy as cp
+from ..utils.para_FFT import para_FFT
+# from cupyx.scipy.sparse import coo_matrix
+
+def aperture_cateye(sigma, f, radius, r_MAX):
+    _, M, _, _, delta, _ = para_FFT(r_MAX)  # 获取FFT参数
+    m1, m2 = cp.meshgrid(cp.linspace(-M//2, M//2 - 1, M), cp.linspace(-M // 2, M // 2 - 1, M))  # 生成采样点下标
+    # T = np.zeros((M, M))  # 初始化边界函数矩阵
+    T = cp.zeros((M, M))
+    a = 0.5  # 圆面对应圆心坐标
+    b = 0.5
+    D = 2 * f * cp.tan(sigma)  # 计算D
+    B = radius**2  # 计算B
+
+    # 计算满足条件的索引并设置T的相应位置为1
+    # i = np.where((((m1 + a) * delta) ** 2 + ((m2 + b) * delta) ** 2 <= B) &
+    #            (((m1 + a) * delta) ** 2 + ((m2 + b) * delta - D) ** 2 <= B))
+    condition = (((m1 + a) * delta) ** 2 + ((m2 + b) * delta) ** 2 <= B) &\
+    (((m1 + a) * delta) ** 2 + ((m2 + b) * delta - D) ** 2 <= B)
+    T[condition] = 1
+    return T
+    # 获取非零元素的行列索引
+    # rows, cols = cp.where(condition)
+
+    # data = cp.ones(rows.size, dtype=cp.float32) 
+    # coo_T = coo_matrix(
+    #     (data, (rows, cols)),  # 注意参数格式：(data, (rows, cols))
+    #     shape=(M, M)
+    # )
+    # csr_T = coo_T.tocsr()
+    # return csr_T
+def aperture_gain(sigma, f, radius, r_CatEye):
+    _, M, _, _, delta, _ = para_FFT(r_CatEye)
+    m1, m2 = cp.meshgrid(cp.linspace(-M // 2, M // 2 - 1, M), cp.linspace(-M // 2, M // 2 - 1, M))
+    T = cp.zeros((M, M))
+    D = 2 * f * cp.tan(sigma)
+    a = 0.5
+    b = 0.5
+
+    # 计算角锥棱镜对应圆面位置
+    # i = np.where((m1 + a) ** 2 * delta ** 2 + (m2 + b) ** 2 * delta ** 2 <= radius ** 2)
+    # T[i] = 1
+    condition = ((m1 + a) ** 2 * delta ** 2 + ((m2 + b)  * delta-D/2 )** 2 <= radius ** 2)
+    T[condition] = 1
+    return T
+    # rows, cols = cp.where(condition)
+    # data = cp.ones(rows.size, dtype=cp.float32) 
+    # coo_T = coo_matrix(
+    #     (data, (rows, cols)),  # 注意参数格式：(data, (rows, cols))
+    #     shape=(M, M)
+    # )
+    # csr_T = coo_T.tocsr()
+    # return csr_T
+
+
+def cal_all_aperture(data,r_max,angle_1,angle_2):
+
+    aperture_all=[]
+
+    # for (index,item) in data['modelAttributeList']:
+    for index, item in enumerate(data['modelAttributeList']):
+        angle = angle_1 if index<=4 else angle_2
+        #判断是镜子还是介质
+        if item['type']=='lens' :
+            aperture=aperture_cateye(angle,item['focalLength'],item['radius'],r_max)
+            #计算的结果都放入GPU中去储存
+            aperture_all.append(aperture.astype(cp.float32))
+
+        elif item['type']=='medium':
+            #这里同样写死第二个参数，由于增益介质孔径计算与其两边透镜的焦距有关
+            aperture=aperture_gain(angle,data['modelAttributeList'][1]['focalLength'],item['radius']*np.cos(angle),r_max)
+            #计算的结果放入GPU中去储存
+            aperture_all.append(aperture.astype(cp.float32))
+        else :
+            continue
+    return aperture_all
