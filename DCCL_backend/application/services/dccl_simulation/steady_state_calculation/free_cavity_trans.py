b957058b7cabe2baf84a2feeeadac404a4700802
adsfdagfqfa <2151767@tongji.edu.cn>
2025-06-26 13:58:35 +0800

    删除无用文件

--- DCCL_backend/application/services/dccl_simulation/steady_state_calculation/free_cavity_trans.py
+++
@@ -1,6 +0,0 @@
-from ..utils import cal_field_transition
-
-
-def free_cavity_trans(U_pre, H_fsdf, B_CatEye2, B_CatEye3):
-    U = cal_field_transition(U_pre, H_fsdf, B_CatEye2, B_CatEye3)  # M2-L3入射表面
-    return U
--- DCCL_backend/application/services/dccl_simulation/utils/cal_trans_factor.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import cupy as cp
-
-def cal_trans_factor(U1, U2):
-    U3 = cp.abs(U1) ** 2
-    U4 = cp.abs(U2) ** 2
-    V = cp.sum(U3) / cp.sum(U4)
-    return V
