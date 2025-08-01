4895796870928f65796f0f6769bb4355afb3496b
# <#>
2025-08-01 23:13:43 +0800

    修改配置文件

--- DCCL_backend/application/config.py
+++ DCCL_backend/application/config.py
@@ -17,7 +17,7 @@ class Config:
     # 数据库配置
     
     #Redis数据库的URL，格式为redis://:password@localhost:6379/0
-    REDIS_URL = "redis://:123456@127.0.0.1:6379/0"
+    REDIS_URL = "redis://:123456@redis-service:6379/0"
     REDIS_DEFAULT_EXPIRE = 18000  # 5 小时
      # 日志配置
     LOG_TO_CONSOLE = True   # 开发环境输出到控制台
