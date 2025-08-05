3751ab577cb92334ca0aaacbd7985aecb9a6d15a
# <#>
2025-08-05 23:35:28 +0800

    修改result部分接口

--- DCCL_backend/application/handlers/result.py
+++ DCCL_backend/application/handlers/result.py
@@ -11,16 +11,15 @@ matplotlib.use('Agg')  # 使用非交互式后端
 import matplotlib.pyplot as plt
 import mpld3
 
-@routes.route("/picture/<string:key>",methods=['GET'])
-def getPicture(key):
-    token = request.cookies.get('token')
-    # user_id=get_uuid_from_token(token)
-    if not token or not verify_jwt_token(token):
-        resp = make_response('认证错误')
-        return resp
-    mat_bytes= get_redis_data(key)
-    mat_buffer = io.BytesIO(mat_bytes)
-    mat_data = np.abs(sio.loadmat(mat_buffer)['matrix'])
+@routes.route("/picture/<string:path>",methods=['GET'])
+def getPicture(path):
+    if not g.user_id:
+        # 生成新用户
+        return make_response('token未上传或验证失败，请重新上传参数', 400)
+    # mat_bytes= get_redis_data(path)
+    # mat_buffer = io.BytesIO(mat_bytes)
+    # mat_data = np.abs(sio.loadmat(mat_buffer)['matrix'])
+    mat_data =np.abs(sio.loadmat(path)['matrix'])
     plt.figure()
     img=plt.imshow(mat_data, cmap='jet', interpolation='nearest')
     plt.colorbar(img)
@@ -30,19 +29,28 @@ def getPicture(key):
     # 返回HTML内容
     return jsonify({'html': html_content})
 
-@routes.route("/download/<string:key_id>",methods=['GET'])
-def download(key_id):
-    token = request.cookies.get('token')
+@routes.route("/download/<string:path>",methods=['GET'])
+def download(path):
+    # token = request.cookies.get('token')
     # user_id=get_uuid_from_token(token)
-    if not token or not verify_jwt_token(token):
-        resp = make_response('认证错误')
-        return resp
-    mat_bytes= get_redis_data(key_id)
-    mat_buffer = io.BytesIO(mat_bytes)
-    mat_buffer.seek(0)  # 确保指针在文件开头
+    # if not token or not verify_jwt_token(token):
+    #     resp = make_response('认证错误')
+    #     return resp
+    # mat_bytes= get_redis_data(key_id)
+    # mat_buffer = io.BytesIO(mat_bytes)
+    # mat_buffer.seek(0)  # 确保指针在文件开头
 
-    # 设置 MIME 类型和文件名
-    mimetype = "application/octet-stream"  # 通用二进制文件类型
-    filename = f"file.mat"  # 客户端看到的文件名，带 .mat 扩展名
+    # # 设置 MIME 类型和文件名
+    # mimetype = "application/octet-stream"  # 通用二进制文件类型
+    # filename = f"file.mat"  # 客户端看到的文件名，带 .mat 扩展名
 
-    return send_file(mat_buffer, mimetype=mimetype, as_attachment=True, download_name=filename)
+    # return send_file(mat_buffer, mimetype=mimetype, as_attachment=True, download_name=filename)
+    if not g.user_id:
+   
+        return make_response('token未上传或验证失败，请重新上传参数', 400)
+    return send_file(
+        path,
+        mimetype="application/octet-stream",
+        as_attachment=True,
+        download_name="file.mat"
+    )
