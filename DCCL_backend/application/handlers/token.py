876cf11718d7cbc8b3bbb138a008dbd5da86ff18
adsfdagfqfa <#>
2025-08-28 13:39:58 +0800

    修改token的domain

--- DCCL_backend/application/handlers/token.py
+++ DCCL_backend/application/handlers/token.py
@@ -15,13 +15,21 @@ def token_init():
     user_id = str(uuid.uuid4())
     new_token = generate_jwt_token(user_id)
     logger.info("新用户:%s",user_id)
+
+     # 动态取客户端请求的 Host，去掉端口
+    host = request.headers.get('Host', 'localhost')        # 1.94.185.7:12000
+    domain = host.split(':')[0]   
+    
+    secure_flag = request.is_secure
     resp = make_response('token生成成功')
     resp.set_cookie(
         'token',
         new_token,
         httponly=True,
-        secure=True,  # 生产环境必须开启
-        samesite='Lax'
+        secure=secure_flag,     # True（HTTPS）/False（HTTP）
+        samesite='Lax',
+        domain=domain,
+        path='/'
     )
     return resp
     
